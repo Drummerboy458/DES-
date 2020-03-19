8c98aeb834bf84be992446f98c6ac21679b63c3f
falalala <1664032805@qq.com>
2020-03-20 01:22:03 +0800

    Update main.cpp

--- Des_Source/main.cpp
+++ Des_Source/main.cpp
@@ -6,11 +6,11 @@ int main()
 {
     Des des;
     string s= "Hello World!";
-	string k = "12345678";
+    string k = "12345678";
     cout<<"密钥："<<k<<endl;
     cout<<"内容："<<s<<endl;
     des.setKey(k);
-	string cypher = des.encode(s);
+    string cypher = des.encode(s);
     string plain = des.decode(cypher);
     cout<<"Encode:"<<cypher<<endl;
     cout<<"Decode:"<<plain<<endl;
